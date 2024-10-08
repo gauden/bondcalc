759b1ab7a449472baf582ecc5f303ace245444ad
Gauden Galea <workersgae@gmail.com>
2024-10-08 15:50:12 +0200

    Remove double st.form statement (bug fix)

--- app.py
+++ app.py
@@ -55,10 +55,6 @@ st.title("Bond YTM and RR Calculator")
 with st.form("data_entry_form"):
     st.header("Enter Bond Details")
 
-# Data Entry Section
-with st.form("data_entry_form"):
-    st.header("Enter Bond Details")
-
     # Create two columns
     col1, col2 = st.columns(2)
 
